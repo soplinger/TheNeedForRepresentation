d7c7b85d15f94a0e1ab0ced446deaf475280423d
soplinger <solekibbles4@gmail.com>
2018-09-27 20:13:58 -0400

    yesterday i turned on my lamp and then a moth flew into it

--- Main.java
+++ Main.java
@@ -1,8 +1,45 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+            //gather user input
+            System.out.println("Input the word you would like to be encrypted: ");
+            Scanner kbInput1 = new Scanner(System.in);
+            String userInput = kbInput1.next();
+
+        //random point in word
+        int randUser = (int)(Math.random()*userInput.length());
+        String word2= userInput.substring(randUser)+userInput.substring(0,randUser);
+
+        //starts random percent
+        int rand1 = (int)(Math.random()*5);
+
+        //encrypts user input
+        for(int j=1;j<=userInput.length();j++)
+        {
+            String userOutput;
+            char out= word2.charAt(j-1);
+            if(rand1%5==0){
+                userOutput=Integer.toBinaryString(out);
+            }
+            else if(rand1%5==1){
+            userOutput=Integer.toOctalString(out);
+            }
+            else if(rand1%5==2){
+                userOutput=Integer.toString(out);
+            }
+            else if(rand1%5==3){
+                userOutput= Integer.toHexString(out);
+            }
+            else{
+                userOutput = Character.toString(out);
+            }
+            rand1++;
+            System.out.print(userOutput);
+        }
+
+
     }
 }
